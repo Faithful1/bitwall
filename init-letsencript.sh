ce3ccd2a49adca732bea483c8ff9114e500b774b
logicboy <faithfulanere@gmail.com>
2020-01-10 17:47:37 +0800

    certbot cleanup

--- init-letsencript.sh
+++ init-letsencript.sh
@@ -2,7 +2,7 @@
 
 domains=(faithbit.tk www.faithbit.tk)
 rsa_key_size=4096
-data_path="./var/data/certbot"
+data_path="/var/data/certbot/conf"
 email="faithful@infused.io" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
